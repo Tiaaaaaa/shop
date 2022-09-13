7d0801ae272e7151789a1accd7849c2e5d85c4b4
tia <tia.gotty@gmail.com>
2022-09-13 15:13:34 +0200

    funziona il routing, da qua si inizia a fare le cose sensate

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const storage = require(path.join(__dirname, 'backend/server/storage'))
 const books = require(path.join(__dirname, 'backend/server/books'))
 let routing = require(path.join(__dirname, 'frontend/routing'))
 
-console.log(routing.function)
+setRoutes(app);
 
 const StormDB = require("stormdb");
 const cfRegEx = /^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i
--- frontend/routing.js
+++ frontend/routing.js
@@ -6,26 +6,26 @@ module.exports = setRoutes = (app) => {
      * pagina iniziale
      */
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend//pages/index.html"));
+        res.sendFile(path.join(__dirname, "pages/index.html"));
     });
 
     /**
      * pagina del carico
      */
     app.get('/carico', (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend/pages/carico.html"));
+        res.sendFile(path.join(__dirname, "pages/carico.html"));
     });
 
     /**
      * pagina dello scarico
      */
     app.get('/scarico', (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend/pages/scarico.html"));
+        res.sendFile(path.join(__dirname, "pages/scarico.html"));
     });
 
     /**
      * Esposizione delle risorse
      */
-    app.use("/res", express.static(path.join(__dirname, 'frontend')));
+    app.use("/res", express.static(__dirname));
 
 }
