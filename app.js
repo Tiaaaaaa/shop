86da6bd1305f5434a033d348efc5d87be5dd47c2
tia <tia.gotty@gmail.com>
2022-09-13 18:20:44 +0200

    oggetti nel ddl ricostruiti con la sintassi corretta, ora funzionanti

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app = express();
 const routing = require('./frontend/routing');
 setRoutes(app);
 
+const storageInt = require("./backend/server/storage/requestes");
+
 const StormDB = require("stormdb");
 const cfRegEx = /^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i
 
@@ -37,7 +39,15 @@ db.default({
         }
     ],
     "storage": [{
-        "book": { "isbn": 1, "title": "ciao", "price": 2 },
+        "book": { 
+            "isbn": 1, 
+            "title": "ciao", 
+            "price": 2,
+            "subject": "math",
+            "volume": "1",
+            "publisher": "sto cazzo",
+            "section": "1 Chimica"
+        },
         "seller": { "cf": "RRGMTT01P22F119U", "id": 0 },
         "buyer": null,
         "buyDate": new Date("August 2, 2022"),
--- backend/DB/ddl.js
+++ backend/DB/ddl.js
@@ -1,13 +1,6 @@
 // Oggetto che definisce i libri 
-exports.Book = {
-    isbn: Number,
-    subject: String,
-    title: String,
-    volume: String,
-    publisher: String,
-    price: Number,
-    section: String,
-    Book(isbn, subject, title, volume, publisher, price, section) {
+exports.Book = class {
+    constructor(isbn, subject, title, volume, publisher, price, section) {
         this.isbn = isbn;
         this.subject = subject;
         this.title = title;
@@ -19,32 +12,22 @@ exports.Book = {
 }
 
 // Oggetto che definisce i clienti
-exports.User = {
-    cf: String,
-    id: Number,
-    User(cf) {
+exports.User = class{
+    constructor(cf) {
         this.cf = cf;
+        this.id = undefined; 
     }
-
 }
 
 // Oggetto che definisce le entry del magazzino
-exports.Storage = {
-    book: this.book,
-    seller: this.client,
-    buyer: this.client,
-    buyDate: Date,
-    sellDate: Date,
-    state: Boolean,
-    Storage (book, seller, buyDate, state) {
+exports.Storage =  class{
+    constructor(book, seller, buyDate, state) {
         this.book = book;
         this.buyDate = buyDate;
         this.seller = seller;
         this.state = state;
         this.buyer = null;
         this.buyDate = null;
-    },
-
-    a: (a) => {console.log(a)}
+    }
 }
 
