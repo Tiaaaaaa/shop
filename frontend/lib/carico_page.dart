bf6c469ceeac9ec3b67521592198591024b34cc1
Mattia <mattia.arrigotti@studenti.unimi.it>
2023-03-14 12:00:07 +0100

    pagina carico con titoli e separazioni fatte bene

--- frontend/lib/carico_page.dart
+++ frontend/lib/carico_page.dart
@@ -16,57 +16,76 @@ class CaricoPage extends StatelessWidget {
             )),
         body: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Container(
-            padding: EdgeInsets.only(top: 100, bottom: 100, left: 50),
             width: 400,
             decoration: BoxDecoration(
                 border: Border(
-                    right: BorderSide(color: secundaryColor, width: 7),
-                    bottom: BorderSide(color: secundaryColor, width: 7))),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "titolo: tua madre",
-                  style: defaultTextStyle,
+              right: BorderSide(color: secundaryColor, width: 7),
+            )),
+            child: Scaffold(
+                appBar: AppBar(
+                  title: const Text("Descrizione del libro"),
+                  automaticallyImplyLeading: false,
+                  shadowColor: const Color(0),
+                  backgroundColor: secundaryColor,
                 ),
-                Text(
-                  "autore: tua madre",
-                  style: defaultTextStyle,
-                ),
-                Text(
-                  "materia: tua madre",
-                  style: defaultTextStyle,
-                ),
-                Text(
-                  "anno: tua madre",
-                  style: defaultTextStyle,
-                ),
-                Text(
-                  "isbn: tua madre",
-                  style: defaultTextStyle,
-                ),
-                Text(
-                  "prezzo: tua madre",
-                  style: defaultTextStyle,
-                ),
-              ],
-            ),
+                body: Padding(
+                    padding: const EdgeInsets.only(left: 50, top: 50),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "titolo: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                        Text(
+                          "autore: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                        Text(
+                          "materia: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                        Text(
+                          "anno: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                        Text(
+                          "isbn: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                        Text(
+                          "prezzo: tua madre",
+                          style: defaultTextStyle,
+                        ),
+                      ],
+                    ))),
           ),
           Expanded(
             child: CustomScrollView(
               slivers: [
+                SliverAppBar(
+                    title: const Text("Acquistati / Nel carrello"),
+                    pinned: true,
+                    backgroundColor: secundaryColor,
+                    automaticallyImplyLeading: false),
                 SliverList(
                   delegate: SliverChildBuilderDelegate(
                       (BuildContext context, int index) {
                     return Container(
-                      height: 30,
-                      margin: const EdgeInsets.only(bottom: 5),
-                      decoration: BoxDecoration(
-                        color: primaryColor,
-                      ),
-                      child: Center(child: Text(index.toString())),
-                    );
+                        height: 30,
+                        margin: const EdgeInsets.only(bottom: 5),
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Text("titolo"),
+                            Text("materia"),
+                            Text("prezzo")
+                          ],
+                        ));
                   }, childCount: 20),
                 )
               ],
@@ -76,9 +95,14 @@ class CaricoPage extends StatelessWidget {
             width: 400,
             decoration: BoxDecoration(
                 border:
-                    Border(left: BorderSide(color: secundaryColor, width: 10))),
+                    Border(left: BorderSide(color: secundaryColor, width: 8))),
             child: CustomScrollView(
               slivers: [
+                SliverAppBar(
+                    title: const Text("disponibili"),
+                    pinned: true,
+                    backgroundColor: secundaryColor,
+                    automaticallyImplyLeading: false),
                 SliverList(
                   delegate: SliverChildBuilderDelegate(
                       (BuildContext context, int index) {
--- frontend/lib/variables.dart
+++ frontend/lib/variables.dart
@@ -4,4 +4,4 @@ Color primaryColor = const Color.fromRGBO(207, 225, 136, 1);
 Color secundaryColor = const Color.fromRGBO(133, 148, 74, 1);
 
 TextStyle defaultTextStyle =
-    TextStyle(color: secundaryColor, fontFamily: "Helvetica",);
+    TextStyle(color: secundaryColor, fontFamily: "Helvetica");
